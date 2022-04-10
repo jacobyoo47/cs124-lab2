730e5dcf2b55da87d367f4b630676ae939476244
Jacob Yoo <jayoo@purestorage.com>
2022-04-10 16:01:38 -0700

    Add focus trapping to modals

--- src/Modal.js
+++ src/Modal.js
@@ -1,36 +1,39 @@
 import {FaRegWindowClose} from 'react-icons/fa';
+import FocusTrap from 'focus-trap-react'
 import './Modal.css'
 
 
 function Modal(props) {
     return (
-        <div className="modal">
-            <div className="content">
-                <div className="header">
-                    <h4 className="title">{props.title}</h4>
-                    <button className="modal-close-button" onClick={() => {
-                        props.onCancel();
-                    }}>
-                        <FaRegWindowClose/>
-                    </button>
-                </div>
-                <div className="body">
-                    {props.children}
-                </div>
-                <div className="footer">
-                    <button className="modal-confirm-button" onClick={() => {
-                        props.onConfirm();
-                    }}>
-                        {props.confirmText}
-                    </button>
-                    <button className="modal-cancel-button" onClick={() => {
-                        props.onCancel();
-                    }}>
-                        {props.cancelText}
-                    </button>
+        <FocusTrap>
+            <div className="modal">
+                <div className="content">
+                    <div className="header">
+                        <h4 className="title">{props.title}</h4>
+                        <button className="modal-close-button" onClick={() => {
+                            props.onCancel();
+                        }}>
+                            <FaRegWindowClose/>
+                        </button>
+                    </div>
+                    <div className="body">
+                        {props.children}
+                    </div>
+                    <div className="footer">
+                        <button className="modal-confirm-button" onClick={() => {
+                            props.onConfirm();
+                        }}>
+                            {props.confirmText}
+                        </button>
+                        <button className="modal-cancel-button" onClick={() => {
+                            props.onCancel();
+                        }}>
+                            {props.cancelText}
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </FocusTrap>
     );
 }
 

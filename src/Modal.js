b055ec1fa376893bc74c521446c65f5c5e197658
Jacob Yoo <jayoo@purestorage.com>
2022-04-10 17:45:46 -0700

    Update design

--- src/Modal.js
+++ src/Modal.js
@@ -1,39 +1,36 @@
 import {FaRegWindowClose} from 'react-icons/fa';
-import FocusTrap from 'focus-trap-react'
 import './Modal.css'
 
 
 function Modal(props) {
     return (
-        <FocusTrap>
-            <div className="modal">
-                <div className="content">
-                    <div className="header">
-                        <h4 className="title">{props.title}</h4>
-                        <button autoFocus className="modal-close-button" aria-label={props.cancelText} onClick={() => {
-                            props.onCancel();
-                        }}>
-                            <FaRegWindowClose/>
-                        </button>
-                    </div>
-                    <div className="body">
-                        {props.children}
-                    </div>
-                    <div className="footer">
-                        <button className="modal-confirm-button" onClick={() => {
-                            props.onConfirm();
-                        }}>
-                            {props.confirmText}
-                        </button>
-                        <button className="modal-cancel-button" onClick={() => {
-                            props.onCancel();
-                        }}>
-                            {props.cancelText}
-                        </button>
-                    </div>
+        <div className="modal">
+            <div className="content">
+                <div className="header">
+                    <h4 className="title">{props.title}</h4>
+                    <button autoFocus className="modal-close-button" aria-label={props.cancelText} onClick={() => {
+                        props.onCancel();
+                    }}>
+                        <FaRegWindowClose/>
+                    </button>
+                </div>
+                <div className="body">
+                    {props.children}
+                </div>
+                <div className="footer">
+                    <button className="modal-confirm-button" onClick={() => {
+                        props.onConfirm();
+                    }}>
+                        {props.confirmText}
+                    </button>
+                    <button className="modal-cancel-button" onClick={() => {
+                        props.onCancel();
+                    }}>
+                        {props.cancelText}
+                    </button>
                 </div>
             </div>
-        </FocusTrap>
+        </div>
     );
 }
 
